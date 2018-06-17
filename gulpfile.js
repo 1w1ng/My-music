85d237cb68a129cc2b3bf7f31e9c3ce6e3aa6f95
1w1ng <921754984@qq.com>
2018-06-17 15:09:55 +0800

    修改配置文件

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
-var gulp = require('gulp');
-
-
-var cssnano = require('gulp-cssnano')    // 合并文件
-var htmlmin = require('gulp-htmlmin')    // html压缩
-var concat = require('gulp-concat')      // CSS压缩
-var uglify = require('gulp-uglify')      // js压缩
-var clean = require('gulp-clean')        //清空文件夹
-
+var gulp = require('gulp')
+var htmlmin = require('gulp-htmlmin')         // html压缩
+var concat = require('gulp-concat')           // 合并文件
+var rename = require('gulp-rename')           // 重命名
+var cssnano = require('gulp-cssnano')         // CSS压缩
+var jshint = require('gulp-jshint')           //js代码规范性检查
+var uglify = require('gulp-uglify')           // js压缩
+var imagemin = require('gulp-imagemin')       //图片压缩
+var clean = require('gulp-clean')             //清空文件夹
 
 gulp.task('html', function () {
   return gulp.src('src/*.html')
@@ -15,17 +15,31 @@ gulp.task('html', function () {
 })
 
 gulp.task('css', function () {
-  gulp.src('./src/css/*.css')
-      .pipe(concat('index-merge.css'))
-      .pipe(cssnano())
-      .pipe(gulp.dest('dist/css'))
-});
+  return gulp.src('src/css/*.css')
+    .pipe(concat('merge.css'))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(cssnano())
+    .pipe(gulp.dest('dist/css/'))
+})
 
 gulp.task('js', function (argument) {
   return gulp.src('src/js/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
     .pipe(concat('merge.js'))
+    .pipe(rename({
+      suffix: '.min'
+    }))
     .pipe(uglify())
     .pipe(gulp.dest('dist/js/'));
+});
+
+gulp.task('img', function (argument) {
+  gulp.src('src/imgs/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/imgs'));
 });
 
 gulp.task('clear', function () {
@@ -33,4 +47,4 @@ gulp.task('clear', function () {
     .pipe(clean());
 });
 
-gulp.task('build', ['html','css', 'js']);
+gulp.task('build', ['html', 'css', 'js', 'img']);
